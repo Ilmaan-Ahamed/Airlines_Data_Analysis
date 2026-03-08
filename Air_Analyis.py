0facc93494b23c96e7427702e0e06bb880ffb1ff
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-03-09 01:24:40 +0530

    data cleaning ....

--- Air_Analyis.py
+++ Air_Analyis.py
@@ -8,3 +8,32 @@ print("--------- The All Dataset list ---------")
 print(df)
 print("-------------------")
 
+                    # Cleaning the data 
+
+df.drop( columns= 'index' , inplace=True)
+print(df.drop)
+print("-------------------")
+
+# Get Statistical summary about the dataset
+print("--------- Get Statistical summary about the dataset ---------")
+print(df.describe)
+print("-------------------")
+
+# A List By Duration
+print(" --------- A List By Duration ---------")
+print(df[df ['duration'] == 49.830000])
+print()
+
+print(df[df ['duration'] == 0.830000])
+print()
+
+print("-------------------")
+
+# A List By Pirce
+print(" --------- A List By Price ---------")
+print(df[df['price'] == 123071.000000])
+print()
+
+print(df[df['price'] == 1105.000000])
+print()
+print("-------------------")
