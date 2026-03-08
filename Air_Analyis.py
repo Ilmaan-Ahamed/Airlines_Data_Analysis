74fce1cf3cf4fd1f99de79c2e8d1153256300f39
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-03-09 01:21:08 +0530

    the data cleaning works starts

--- Air_Analyis.py
+++ Air_Analyis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df=pd.read_csv(r"E:\Projects\25'24'23\Data Science\Data Analytics & Visualization\Air Lines Data Analytics\airlines_flights_data.csv")
-print("The All Dataset list")
+print("--------- The All Dataset list ---------")
 print(df)
 print("-------------------")
+
