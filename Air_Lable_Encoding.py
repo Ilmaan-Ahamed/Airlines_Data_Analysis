557939ef6031b67c17e1a8e63261e0539e65b92a
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-04-02 21:23:25 +0530

    importing the logic depencnes

---
+++ Air_Lable_Encoding.py
@@ -0,0 +1,6 @@
+import numpy as np from sklearn.preprocessing import LabelEncoder
+
+df=pd.read_csv(r"E:\Projects\25'24'23\Data Science\Data Analytics & Visualization\Air Lines Data Analytics\airlines_flights_data.csv")
+print("--------- The All Dataset list ---------")
+print(df)
+print("-------------------")
