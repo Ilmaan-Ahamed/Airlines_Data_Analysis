80ee759a9ed8930d164f512a85e1d6942cc15207
Ilmaan-Ahamed <ilmaanahamed456@gmail.com>
2026-04-02 22:27:12 +0530

    Feature Engineering + Label Encoding

--- Air_Lable_Encoding.py
+++ Air_Lable_Encoding.py
@@ -1,6 +1,10 @@
-import numpy as np from sklearn.preprocessing import LabelEncoder
+import numpy as np 
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
 
+# load DataSet
 df=pd.read_csv(r"E:\Projects\25'24'23\Data Science\Data Analytics & Visualization\Air Lines Data Analytics\airlines_flights_data.csv")
 print("--------- The All Dataset list ---------")
-print(df)
-print("-------------------")
+
+# Check Data info
+print(df.info)
